120ea69d2545b0d4e6f6c788378116b31ae55020
Shevy Mwine <andrewshevymwine@gmail.com>
2026-03-24 07:46:14 +0300

    modified:   back/config/server.js to enable CORS btn strpi & render

--- back/config/server.js
+++ back/config/server.js
@@ -1,10 +1,17 @@
+// back/config/server.js
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
   },
+  // Trust the proxy headers (Render sets X-Forwarded-Proto)
+  proxy: true,
+  // Set the public URL explicitly (use your full HTTPS URL)
+  url: env('PUBLIC_URL', 'https://mukatale-ug-com.onrender.com'),
 });
