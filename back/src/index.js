d53596185789b7ee092877caf4e62abf474698c1
Shevy Mwine <andrewshevymwine@gmail.com>
2026-03-27 10:39:45 +0300

    For preventing strapi auto refresh on each redeploy

--- back/src/index.js
+++ back/src/index.js
@@ -1,16 +1,40 @@
 'use strict';
 
 module.exports = {
-  register({ strapi }) {
-    // 🔥 This fixes "Cannot send secure cookie over unencrypted connection"
-    // on Render / any proxy that terminates HTTPS
-    strapi.server.use(async (ctx, next) => {
-      if (ctx.req?.socket) {
-        ctx.req.socket.encrypted = true;
-      }
-      await next();
-    });
-  },
+  async register({ strapi }) {},
 
-  bootstrap({ strapi }) {},
+  async bootstrap({ strapi }) {
+    const publicRole = await strapi
+      .query('plugin::users-permissions.role')
+      .findOne({ where: { type: 'public' } });
+
+    const permissions = await strapi
+      .query('plugin::users-permissions.permission')
+      .findMany({ where: { role: publicRole.id } });
+
+    const contentTypes = [
+      'api::market.market',
+      'api::vendor.vendor', 
+      'api::product.product',
+      'api::category.category',
+    ];
+
+    for (const contentType of contentTypes) {
+      for (const action of ['find', 'findOne']) {
+        const exists = permissions.some(
+          p => p.action === `${contentType}.${action}`
+        );
+        if (!exists) {
+          await strapi
+            .query('plugin::users-permissions.permission')
+            .create({
+              data: {
+                action: `${contentType}.${action}`,
+                role: publicRole.id,
+              },
+            });
+        }
+      }
+    }
+  },
 };
