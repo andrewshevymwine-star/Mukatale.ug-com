eea258c77006c637914a6242734d7f6d18bca635
Shevy Mwine <andrewshevymwine@gmail.com>
2026-03-30 13:05:31 +0300

    fix: session proxy and CORS for production

--- back/config/middlewares.js
+++ back/config/middlewares.js
@@ -4,7 +4,12 @@ module.exports = [
   {
     name: 'strapi::cors',
     config: {
-      origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+      origin: [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://mukatale-ug-com.onrender.com',
+        'https://mukatale-ug-com-1.onrender.com',
+      ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
       keepHeaderOnError: true,
@@ -14,7 +19,14 @@ module.exports = [
   'strapi::logger',
   'strapi::query',
   'strapi::body',
-  'strapi::session',
+  {
+    name: 'strapi::session',
+    config: {
+      proxy: true,
+      secure: true,
+      sameSite: 'none',
+    },
+  },
   'strapi::favicon',
   'strapi::public',
 ];
